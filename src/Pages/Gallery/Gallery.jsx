befba9da74fc1c4ff2c16c5619020f8156346160
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2025-05-13 20:12:22 +0600

    simple design updated

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -8,29 +8,28 @@ const Gallery = () => {
 
   return (
     <div className="container h-full ">
-      <div className="foreground">
+      <div className="foreground flex justify-center items-center">
         <h2 className="text-6xl text-[#2e2e2e2c] font-extrabold  background-text ">
           Food Gallary
         </h2>
 
         <div className="images space-y-4">
           {dbData.map((loadImages) => (
-
-<div>
-              <div class="card">
-                <div class="card-inner">
-                  <div class="card-front">
-                    <img src={loadImages.imgURL} alt="" />
-                  </div>
-                  <div class="card-back">
-                    <h3 className="font-bold">{loadImages.foodName}</h3>
-                    <p>{loadImages.description}</p>
-                    <p> {loadImages.imgURL} </p>
-                  </div>
-                </div>
-              </div>
+            <div className="images">
+              <a
+                data-tooltip-id="my-tooltip-data-html"
+                className="text-"
+                data-tooltip-html={` <div>
+            <ul className="text-lg">
+              <li className='font-bold'>${loadImages.foodName} </li>
+              <li> ${loadImages.description}  </li>
+            </ul>
+          </div>`}
+              >
+                <img src={loadImages.imgURL} alt="" />
+              </a>
+              <Tooltip id="my-tooltip-data-html" />
             </div>
-           
           ))}
         </div>
       </div>
@@ -55,18 +54,35 @@ export default Gallery;
               </div>
             </div> */
 }
-//  <div className="images">
-//               <a
-//                 data-tooltip-id="my-tooltip-data-html"
-//                 className="text-"
-//                 data-tooltip-html={` <div>
-//             <ul className="text-lg">
-//               <li className='font-bold'>${loadImages.foodName} </li>
-//               <li> ${loadImages.description}  </li>
-//             </ul>
-//           </div>`}
-//               >
-//                 <img src={loadImages.imgURL} alt="" />
-//               </a>
-//               <Tooltip id="my-tooltip-data-html" />
-//             </div>
+{/* <div className="images">
+  <a
+    data-tooltip-id="my-tooltip-data-html"
+    className="text-"
+    data-tooltip-html={` <div>
+            <ul className="text-lg">
+              <li className='font-bold'>${loadImages.foodName} </li>
+              <li> ${loadImages.description}  </li>
+            </ul>
+          </div>`}
+  >
+    <img src={loadImages.imgURL} alt="" />
+  </a>
+  <Tooltip id="my-tooltip-data-html" />
+</div>; */}
+
+{
+  /* <div>
+              <div class="card">
+                <div class="card-inner">
+                  <div class="card-front">
+                    <img src={loadImages.imgURL} alt="" />
+                  </div>
+                  <div class="card-back">
+                    <h3 className="font-bold">{loadImages.foodName}</h3>
+                    <p>{loadImages.description}</p>
+                    <p> {loadImages.imgURL} </p>
+                  </div>
+                </div>
+              </div>
+            </div> */
+}
